7e8f4faff997785241ff49b512707a5af381a692
phagoth <olexandr.averyanov@gmail.com>
2015-10-27 14:06:58 +0200

    uncomment part of code

--- main.rb
+++ main.rb
@@ -7,28 +7,28 @@ lib.generate_readers(100)
 lib.generate_orders(100)
 
 
-puts '----------------------'
+puts '======================'
 puts lib.most_popular_books
 puts '----------------------'
 puts lib.most_popular_books(3)
-puts '----------------------'
-puts '----------------------'
+puts '======================'
+puts '======================'
 puts lib.top_readers
 puts '----------------------'
 puts lib.top_readers(3)
-puts '----------------------'
-puts '----------------------'
+puts '======================'
+puts '======================'
 puts lib.popular_books_readers
 puts '----------------------'
 puts lib.popular_books_readers(1)
-puts '----------------------'
+puts '======================'
 
-# path = lib.save_to_files
+path = lib.save_to_files
 
-# p lib
+p lib
 
-# lib2 = Library.new
-# lib2.load_from_files(path)
-# p lib2
+lib2 = Library.new
+lib2.load_from_files(path)
+p lib2
 
-# puts lib == lib2
+puts lib == lib2
